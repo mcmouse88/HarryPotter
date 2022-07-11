d7b21afcc20d14dbb490a431085936f75450f4fa
mcmouse88 <mcmouse88@icloud.com>
2022-07-11 17:57:13 +0200

    injection implementation in UseCaseRepositoryImpl.kt

--- app/src/main/java/com/mcmouse88/harrypotter/data/repository/UseCaseRepositoryImpl.kt
+++ app/src/main/java/com/mcmouse88/harrypotter/data/repository/UseCaseRepositoryImpl.kt
@@ -1,19 +1,17 @@
 package com.mcmouse88.harrypotter.data.repository
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import com.mcmouse88.harrypotter.data.room.mapper.DataBaseModelMapper
+import com.mcmouse88.harrypotter.data.room.repository.RoomRepository
 import com.mcmouse88.harrypotter.data.room.repository.RoomRepositoryImpl
 import com.mcmouse88.harrypotter.domain.entity.Character
 import com.mcmouse88.harrypotter.domain.usecaserepository.UseCaseRepository
 
 class UseCaseRepositoryImpl(
-    application: Application
+    private val roomRepository: RoomRepository,
+    private val mapper: DataBaseModelMapper
 ) : UseCaseRepository {
-
-    private val roomRepository = RoomRepositoryImpl(application)
-    private val mapper = DataBaseModelMapper()
 
     override suspend fun addToFavoriteUseCase(character: Character) {
         val dbModel = mapper.mapEntityToDbModel(character)
--- app/src/main/java/com/mcmouse88/harrypotter/di/DataModule.kt
+++ app/src/main/java/com/mcmouse88/harrypotter/di/DataModule.kt
@@ -2,11 +2,27 @@ package com.mcmouse88.harrypotter.di
 
 import com.mcmouse88.harrypotter.data.network.ApiRepository
 import com.mcmouse88.harrypotter.data.repository.UseCaseRepositoryImpl
+import com.mcmouse88.harrypotter.data.room.mapper.DataBaseModelMapper
+import com.mcmouse88.harrypotter.data.room.repository.RoomRepository
+import com.mcmouse88.harrypotter.data.room.repository.RoomRepositoryImpl
 import com.mcmouse88.harrypotter.domain.usecaserepository.UseCaseRepository
 import org.koin.dsl.module
 
 val dataModule = module {
-    single<UseCaseRepository> { UseCaseRepositoryImpl(application = get()) }
+    single<UseCaseRepository> {
+        UseCaseRepositoryImpl(
+            roomRepository = get(),
+            mapper = get()
+        )
+    }
+
+    single<RoomRepository> {
+        RoomRepositoryImpl(application = get())
+    }
+
+    factory {
+        DataBaseModelMapper()
+    }
 
     single { ApiRepository() }
 }
